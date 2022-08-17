2c5c1f049b5125d1ef0b1e11b4fe016c02a84e29
Crimson2461 <111436689+Crimson2461@users.noreply.github.com>
2022-08-17 14:42:19 +0200

    Remove assert statements

--- src/modeling/glam.py
+++ src/modeling/glam.py
@@ -57,10 +57,10 @@ class MILSingleImageModel(nn.Module):
         top_k_prop_x = crops_x_small[:, :, 0] / h
         top_k_prop_y = crops_x_small[:, :, 1] / w
         # sanity check
-        assert np.max(top_k_prop_x) <= 1.0, "top_k_prop_x >= 1.0"
-        assert np.min(top_k_prop_x) >= 0.0, "top_k_prop_x <= 0.0"
-        assert np.max(top_k_prop_y) <= 1.0, "top_k_prop_y >= 1.0"
-        assert np.min(top_k_prop_y) >= 0.0, "top_k_prop_y <= 0.0"
+        # assert np.max(top_k_prop_x) <= 1.0, "top_k_prop_x >= 1.0"
+        # assert np.min(top_k_prop_x) >= 0.0, "top_k_prop_x <= 0.0"
+        # assert np.max(top_k_prop_y) <= 1.0, "top_k_prop_y >= 1.0"
+        # assert np.min(top_k_prop_y) >= 0.0, "top_k_prop_y <= 0.0"
         # interpolate the crop position from cam_size to x_original
         top_k_interpolate_x = np.expand_dims(np.around(top_k_prop_x * H), -1)
         top_k_interpolate_y = np.expand_dims(np.around(top_k_prop_y * W), -1)
